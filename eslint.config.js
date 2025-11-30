c43fc487780beb4c46266e2f44de4acd51111e7c
unknown <mitchell.perry001@gmail.com>
2025-11-30 03:19:54 +0000

    chore: update ESLint config and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -18,6 +18,23 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      // Allow exports in test files
+      'react-refresh/only-export-components': [
+        'warn',
+        { 
+          allowExportNames: ['render', 'screen', 'fireEvent', 'waitFor', 'within', 'act'],
+          allowConstantExport: true 
+        }
+      ],
+    },
+  },
+  {
+    // Relax rules for test files and hook files (hooks need to export non-components)
+    files: ['**/*.test.{ts,tsx}', '**/test-utils.tsx', '**/__tests__/**/*.{ts,tsx}', '**/hooks/**/*.{ts,tsx}', '**/context/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
